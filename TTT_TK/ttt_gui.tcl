285c8cd16c67a3b4ab767614b5fa88b9445f9c8e
Likhitha Patha <likhitha@vt.edu>
2012-05-01 20:32:04 -0400

    Added the GUI portion of the project in a folder called TTT_TK

---
+++ TTT_TK/ttt_gui.tcl
@@ -0,0 +1,145 @@
+#  Create the main message window
+wm title . "TicTacToe in TCL!" 
+#Set dimensions of the TicTacToe box size                       
+wm geometry . 210x210+300+300     
+#Variable used to draw                    
+canvas .can                                                                 
+
+#consumes a gif files and saves it to a variable 
+set cross [image create photo -file ./cross.gif]    
+set not [image create photo -file ./not.gif]
+# Updates the GUI based on the following integer code values
+# 100 : places X in coordinate (0,0)
+# 101 : places X in coordinate (0,1)
+# 102 : places X in coordinate (0,2) 
+# 110 : places X in coordinate (1,0)
+# 111 : places X in coordinate (1,1)
+# 112 : places X in coordinate (1,2)
+# 120 : places X in coordinate (2,0)
+# 121 : places X in coordinate (2,1)
+# 122 : places X in coordinate (2,2)
+# 200 : places O in coordinate (0,0)
+# 201 : places O in coordinate (0,1)
+# 202 : places O in coordinate (0,2) 
+# 210 : places O in coordinate (1,0)
+# 211 : places O in coordinate (1,1)
+# 212 : places O in coordinate (1,2)
+# 220 : places O in coordinate (2,0)
+# 221 : places O in coordinate (2,1)
+# 222 : places O in coordinate (2,2)
+# 30 : clears the screen
+proc updater {code} {
+	global cross
+	global not
+	switch $code {
+		100 {
+ 			.can create image 40 40 -image $cross
+			pack .can	 
+		}
+		101 {
+ 			.can create image 105 40 -image $cross
+			pack .can	 
+		}
+		102 {
+ 			.can create image 170 40 -image $cross
+			pack .can	 
+		}
+		110 {
+ 			.can create image 40 105 -image $cross
+			pack .can	 
+		}
+		111 {
+ 			.can create image 105 105 -image $cross
+			pack .can	 
+		}
+		112 {
+ 			.can create image 170 105 -image $cross
+			pack .can	 
+		}
+		120 {
+ 			.can create image 40 170 -image $cross
+			pack .can
+		}	 
+		121 {
+ 			.can create image 105 170 -image $cross
+			pack .can
+		}	 
+		122 {
+ 			.can create image 170 170 -image $cross
+			pack .can	 
+		}
+		200 {
+ 			.can create image 40 40 -image $not
+			pack .can	 
+		}
+		201 {
+ 			.can create image 105 40 -image $not
+			pack .can	 
+		}
+		202 {
+ 			.can create image 170 40 -image $not
+			pack .can	 
+		}
+		210 {
+ 			.can create image 40 105 -image $not
+			pack .can	 
+		}
+		211 {
+ 			.can create image 105 105 -image $not
+			pack .can	 
+		}
+		212 {
+ 			.can create image 170 105 -image $not
+			pack .can	 
+		}
+		220 {
+ 			.can create image 40 170 -image $not
+			pack .can
+		}	 
+		221 {
+ 			.can create image 105 170 -image $not
+			pack .can
+		}	 
+		222 {
+ 			.can create image 170 170 -image $not
+			pack .can	 
+		}
+		30 {
+			.can delete "all"
+			.can create rect 10 10 200 200
+			.can create line 70 10 70 200
+			.can create line 140 10 140 200
+			.can create line 10 70 200 70
+			.can create line 10 140 200 140
+			pack .can
+		}	
+		default {
+		}
+	}
+}
+
+# Creates the TicTacToe Grid
+.can create rect 10 10 200 200    
+.can create line 70 10 70 200
+.can create line 140 10 140 200
+.can create line 10 70 200 70   
+.can create line 10 140 200 140 
+pack .can
+
+##exec rm cpp_pipe
+#Makes a named pipe called cpp_pipe
+##exec mkfifo cpp_pipe  
+#opens the pipe as a read/write non-blocking                          
+##set fifo [open "cpp_pipe" {RDWR NONBLOCK}]
+#sets the pipe, in a way its only expecting one integer      
+#fconfigure $fifo -blocking 1 
+#Reads from fifo and calls the updater procedure                   
+proc read_stdin {} {                           
+	#global fifo                     
+	##gets $fifo x
+    gets stdin x
+    #puts $x  	
+    updater $x
+}             
+#Connects the triggering of the named pipe to the read_fifo procedure                        
+fileevent stdin readable read_stdin            
